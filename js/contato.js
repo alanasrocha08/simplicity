2665fa2f9aa27ebdf707fccef000235625bc4e4b
alanasrocha08 <alanasr02@gmail.com>
2024-10-16 10:41:45 -0300

    Busca de cep

--- js/contato.js
+++ js/contato.js
@@ -1,6 +1,7 @@
 //Selecionando os elemetos que serão manipulados
 const formulario = document.querySelector("form");
 const campoCep = formulario.querySelector("#cep");
+const campoTelefone = formulario.querySelector("#telefone");
 const campoEndereco = formulario.querySelector("#endereço");
 const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
@@ -8,8 +9,12 @@ const campoEstado = formulario.querySelector("#estado");
 const botaoBuscar = formulario.querySelector("#buscar");
 const mensagemStatus = formulario.querySelector("#status");
 
+/* Ativação da máscara para telefone e CEP*/
+$(campoTelefone).mask("(00) 0000-00000");
+$(campoCep).mask("00000-000"); 
+
 /* Capturando o clique no botão buscar */
-botaoBuscar.addEventListener("click", function(){
+botaoBuscar.addEventListener("click", async function(){
     //Verificando se o cep NÃO TEM 9 DIGITOS
     if(campoCep.value.length !== 9){
         //Informar o usuário sobre o erro
@@ -23,7 +28,56 @@ botaoBuscar.addEventListener("click", function(){
 
     /* Guardando o valor do cep digitado */
     let cepDigitado = campoCep.value;
+    console.log(cepDigitado);
+    
 
     /* AJAX = Asyncronous JavaScript And XML
     Técnica de comunicação (tranmissão, recebimento) de dados muito usada entre sistemas e tecnologias diferentes.*/
+
+    //Etapa 1: preparar a url contendo o CEP a ser buscado
+    let url = `https://viacep.com.br/ws/${cepDigitado}/json/`;
+    console.log(url);
+    
+    
+    //Etapa 2 : acessar a API (com a url) e aguardar o retorno dela
+    const resposta = await fetch(url);
+    console.log(resposta);
+
+    //Etapa 3: extrair os dados do retorno/resposta
+    const dados = await resposta.json();
+    console.log(dados);
+    
+    //Etapa 4: lidar com os dado (em caso de erro e sucesso)
+    if("erro" in dados){
+        mensagemStatus.innerHTML = "CEP inexistente 😢";
+        mensagemStatus.style.color = "red";
+    } else {
+        mensagemStatus.innerHTML = "CEP encontrado 😃";
+        mensagemStatus.style.color = "blue";
+
+        //Selecionando todos os campos com a classe
+        const campos = formulario.querySelectorAll(".campos-restantes");
+
+        /*Loop/Laço de repetição para acessar CADA campo selecionado e remover a classe fazendo com que cada campo volte a aparecer.*/
+        for(const campo of campos){
+            campo.classList.remove("campos-restantes");
+        }
+        
+        /*Atribuindo os dados para cada campo*/
+
+        //colocar o logradouro como valor do campo endereço
+        campoEndereco.value = dados.logradouro;
+
+        //colocar o bairro como valor do campo bairro
+        campoBairro.value = dados.bairro;
+
+        //colocar a localidade como calor da cidade 
+        campoCidade.value = dados.localidade;   
+
+        //colocar a uf como valor do campo estado
+        campoEstado.value = dados.uf;
+
+    }
+
 }); //final do evento do botão
+
